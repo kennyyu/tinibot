b38098de1b9abecebce204c82652ca169b761d2a
Kenny Yu <yu.kenny124@gmail.com>
2014-04-17 21:37:30 -0400

    serialize struct across wire

--- arduino.py
+++ arduino.py
@@ -1,13 +1,14 @@
 import serial
+import struct
 import time
 
-DEVICE = "/dev/tty.usbmodemfa131"
+DEVICE = "/dev/tty.usbmodemfd121"
 
 ser = serial.Serial(DEVICE, 9600)
 time.sleep(2) # wait for device to initialize itself
 
 while True:
-    request = "hello"
+    request = struct.pack("<III", 1000, 2000, 3000)
     ser.write(request)
     response = ser.readline()
     response = response.strip()
--- arduino_receiver/arduino_receiver.ino
+++ arduino_receiver/arduino_receiver.ino
@@ -1,33 +1,41 @@
-const int len = 5;
-char buf[len];
-int nread = 0;
+const unsigned npins = 3;
+const unsigned pins[npins] = {4, 5, 6};
+
+struct message {
+  uint32_t times[npins];
+};
 
 void setup() {
-  for (int i = 0; i < len; i++) {
-    buf[i] = 0;
-  }
   Serial.begin(9600);
-  pinMode(4, OUTPUT);
+  for (unsigned i = 0; i < npins; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
 }
 
 void loop() {
+  struct message msg;
+  for (unsigned i = 0; i < npins; i++) {
+    msg.times[i] = 0;
+  }
+  char *buf = (char *) &msg;
+  unsigned nread = 0;
+  unsigned len = sizeof(struct message);
+
   while (nread < len) {
     if (Serial.available() > 0) {
       byte result = Serial.readBytes(&buf[nread], len - nread);
       nread += result;
     }
   }
-  String bufstring(buf);
-  String response = "RESPONSE>>> " + bufstring + " world!";
-  for (int i = 0; i < len; i++) {
-    buf[i] = 0;
-  }
-  nread = 0;
+  String response = "RESPONSE>>> " + String(msg.times[0]) + "," + String(msg.times[1]) + "," + String(msg.times[2]);
   Serial.println(response);
   Serial.flush();
-  digitalWrite(4, HIGH);
-  delay(1000);
-  digitalWrite(4, LOW);
+  for (unsigned i = 0; i < npins; i++) {
+    unsigned pin = pins[i];
+    digitalWrite(pin, HIGH);
+    delay(msg.times[i]);
+    digitalWrite(pin, LOW);
+  }
   delay(1000);
 }
 
